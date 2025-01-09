0b5b6ab2f290d7e40c7c64058f83819fe24ac2a7
Littlestar0508 <rist9860@gmail.com>
2025-01-09 15:04:05 +0900

    부모 요소를 생성하고 그 내부에 자식 요소를 할당해 actual dom 에 렌더링

--- public/scripts/react-demo.ts
+++ public/scripts/react-demo.ts
@@ -2,6 +2,7 @@
 
 import React from "../lib/react.js";
 import ReactDOM from "../lib/react-dom/client.js";
+import createElement from "./../lib/own/own-createElement-demo";
 
 // React Version
 // console.log("React Version = ", React.version);
@@ -16,10 +17,22 @@ import ReactDOM from "../lib/react-dom/client.js";
 // React API
 // React.createElement(type, props(속성들의 집합) , ...children)
 
-const heading = React.createElement("h1", {} /* 속성X */, "안녕!");
+const heading = React.createElement("h1", {} /* 속성X */, "안녕!" /* children */);
 const changeButton = React.createElement("button", { type: "button" }, "인사말");
 
-console.group("React 요소 노드");
-console.dir(heading);
-console.dir(changeButton);
-console.groupEnd();
+const parentElement = React.createElement("div", { role: "group" }, heading, changeButton);
+
+// console.group("React 요소 노드");
+// console.dir(heading);
+// console.dir(changeButton);
+// console.groupEnd();
+
+// ReactDOM API (for Web)
+// ReactDOM.createRoot(domElement) -> ReactDOMRoot {render , unmount}
+const rootElement = document.getElementById("react"); // HTMLDivElement
+const reactDomRoot = ReactDOM.createRoot(rootElement); // ReactDOMRoot
+
+// ReactDOMRoot.render(React.ReactElement)
+reactDomRoot.render(parentElement);
+
+// React Native API (for Mobile Native)
