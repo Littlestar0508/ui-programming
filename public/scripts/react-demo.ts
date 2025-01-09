7ef930aae842fd2e436128e3095dfe5603906757
Littlestar0508 <rist9860@gmail.com>
2025-01-09 13:51:12 +0900

    react에서 createElement 메서드 사용법과 ownElement함수를 비슷하게 구현해보기

--- public/scripts/react-demo.ts
+++ public/scripts/react-demo.ts
@@ -1,1 +1,47 @@
 /* ------------------------ React Programming Preview ----------------------- */
+
+import React from "../lib/react.js";
+import ReactDOM from "../lib/react-dom/client.js";
+
+// React Version
+// console.log("React Version = ", React.version);
+
+// ReactDOM/client version
+// console.log("ReactDOM Version = ", ReactDOM.version);
+
+/* -------------------------------------------------------------------------- */
+/*                            Creating Element Node                           */
+/* -------------------------------------------------------------------------- */
+
+// React API
+// React.createElement(type, props(속성들의 집합) , ...children)
+
+const heading = React.createElement("h1", {} /* 속성X */, "안녕!");
+const changeButton = React.createElement("button", { type: "button" }, "인사말");
+
+console.group("React 요소 노드");
+console.dir(heading);
+console.dir(changeButton);
+console.groupEnd();
+
+/* -------------------------------------------------------------------------- */
+/*                         Own CreateElement Function                         */
+/* -------------------------------------------------------------------------- */
+
+function createElement(type, props = {}, ...children) {
+  return {
+    $$typeof: Symbol("own.element"),
+    key: null,
+    type,
+    props: {
+      ...props,
+      children,
+    },
+  };
+}
+
+const ownElement = createElement("div", { id: "own" }, "hello");
+
+console.group("Own 요소 노드");
+console.log(ownElement);
+console.groupEnd();
--- public/scripts/vanilla-demo.ts
+++ public/scripts/vanilla-demo.ts
@@ -9,6 +9,10 @@ const greetingList = Object.values(Greeting); // values
 const greetingKeyLst = Object.keys(Greeting); // keys
 const greetingEntryList = Object.entries(Greeting); // [[key,value] , ...]
 
+/* -------------------------------------------------------------------------- */
+/*                            Creating Element Node                           */
+/* -------------------------------------------------------------------------- */
+
 // 제어 대상 참조X -> 생성
 // const heading = document.querySelector("h1");
 const heading = document.createElement("h1");
@@ -19,6 +23,11 @@ heading.textContent = greetingList.at(0) as string;
 const changeButton = document.createElement("button");
 changeButton.setAttribute("type", "button");
 changeButton.textContent = "인사말";
+
+console.group("DOM 요소 노드");
+console.dir(heading);
+console.dir(changeButton);
+console.groupEnd();
 
 // 버튼에 이벤트 바인딩
 changeButton?.addEventListener("click", handleChangeGreetMessage);
