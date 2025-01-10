07f0f3c864eef4d37ab05796707a7618d5044d9c
Littlestar0508 <rist9860@gmail.com>
2025-01-10 13:53:51 +0900

    계산기 로직

--- public/components/app.ts
+++ public/components/app.ts
@@ -1,8 +1,14 @@
-import React from "../lib/react";
+import React from "../lib/react.js";
+import Calculator from "./calculator";
 
 function App() {
   return React.createElement("div", {
     className: "app",
+    children: [
+      React.createElement(Calculator),
+      React.createElement(Calculator, { elements: [10, 2] }),
+      React.createElement(Calculator, { operator: "+", elements: [10, 2] }),
+    ],
   });
 }
 
