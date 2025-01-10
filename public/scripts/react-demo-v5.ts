843545c149c92a4415f3d1f96ae88176d939b5fd
Littlestar0508 <rist9860@gmail.com>
2025-01-10 09:43:16 +0900

    React 재사용을 위한 함수 생성

--- public/scripts/react-demo-v5.ts
+++ public/scripts/react-demo-v5.ts
@@ -7,13 +7,24 @@ import ReactDOM from "../lib/react-dom/client.js";
 /*                            Creating Element Node                           */
 /* -------------------------------------------------------------------------- */
 
+// 리액트 엘리먼트(반환될 값)
+
+// Heding 컴포넌트
+// const heading = React.createElement("h1", {} /* 속성X */, "안녕!" /* children */);
+
 function Heading() {
   return React.createElement("h1", { children: "안녕!" });
 }
 
+// ChangeButton 컴포넌트
+// const changeButton = React.createElement("button", { type: "button" }, "인사말");
+
 function ChangeButton() {
   return React.createElement("button", { type: "button", children: "인사말" });
 }
+
+// Wrapper 컴포넌트
+// const wrapper = React.createElement(React.Fragment, { role: "group" }, heading, changeButton);
 
 function Wrapper() {
   return React.createElement(
@@ -26,5 +37,8 @@ function Wrapper() {
 
 const rootElement = document.getElementById("react"); // HTMLDivElement
 const reactDomRoot = ReactDOM.createRoot(rootElement); // ReactDOMRoot
+
+// React Element Rendering
+// reactDomRoot.render(wrapper);
 
 reactDomRoot.render(React.createElement(Wrapper));
