442fd1ed46dfd58f2bde9367f9dfc4a86682b1a7
Littlestar0508 <rist9860@gmail.com>
2025-01-10 13:07:34 +0900

    app.ts main.ts 생성

---
+++ public/scripts/main.ts
@@ -0,0 +1,5 @@
+import App from "../components/app";
+import ReactDOM from "../lib/react-dom/client.js";
+import React from "../lib/react.js";
+
+ReactDOM.createRoot(document.getElementById("react")).render(React.createElement(App));
