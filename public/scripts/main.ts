8fcc7befea072733f4aaf06e52236ac82de24c52
Littlestar0508 <rist9860@gmail.com>
2025-01-13 09:21:16 +0900

    폴더 명 변경
    backend, frontend

--- public/scripts/main.ts
+++
@@ -1,5 +0,0 @@
-import App from "../components/app";
-import ReactDOM from "../lib/react-dom/client.js";
-import React from "../lib/react.js";
-
-ReactDOM.createRoot(document.getElementById("react")).render(React.createElement(App));
