d8670e4b35c9476a901807b81487422b652c2f6d
Littlestar0508 <rist9860@gmail.com>
2025-01-13 14:49:13 +0900

    사용자의 접근성 향상을 위한 space와 enter키로 이벤트 동작 구현

---
+++ frontend/components/switch08.ts
@@ -0,0 +1,71 @@
+// --------------------------------------------------------------------------
+// ✅ Switch 컴포넌트 작성(실습)
+// --------------------------------------------------------------------------
+// - [x] switch 역할(role) 설정
+// - [x] aria-checked 속성을 사용해 ON/OFF 상태 설정
+// - [ ] ON/OFF 텍스트를 화면에 표시하더라도 aria-hidden 속성을 사용해 읽지 않도록 설정
+// - [ ] 사용자가 Space, Enter(옵션) 키를 눌렀을 때 작동되도록 설정
+// - [ ] 비활성 상태인 경우, aria-disabled 속성을 사용해 설정
+// --------------------------------------------------------------------------
+import React from "../lib/react.js";
+
+const h = React.createElement;
+
+interface SwitchProps {
+  active?: boolean;
+  disabled?: boolean;
+  showOnOffText?: boolean;
+  children: React.ReactNode;
+  onToggle?: () => void;
+}
+
+function Switch({
+  active = false,
+  disabled = false,
+  showOnOffText = false,
+  onToggle,
+  children,
+}: SwitchProps) {
+  let switchText: "ON" | "OFF" | null = !showOnOffText ? null : active ? "ON" : "OFF";
+
+  let switchTextNode = null;
+
+  if (switchText) {
+    switchTextNode = h("span", { className: "Switch--text", "aria-hidden": true }, switchText);
+  }
+
+  const handleToggle = () => {
+    if (disabled) return;
+    onToggle?.();
+  };
+
+  const handleKeyControl = (e: KeyboardEvent) => {
+    const key = e.code;
+    const pressedShiftKey = !!e.shiftKey;
+    if (!disabled && !pressedShiftKey && (key === "Space" || key === "Enter")) onToggle?.();
+  };
+
+  return h(
+    "div",
+    {
+      role: "switch",
+      "aria-checked": active,
+      "aria-disabled": disabled,
+      tabIndex: 0,
+      className: "Switch",
+      // 이벤트 핸들러를 onClick 속성에 연결
+      // 활성상태 또는 비활성 상태에 따라 함수(기능) 작동 여부 조건 처리
+      onClick: handleToggle,
+      onKeyDown: handleKeyControl,
+    },
+    h("span", { className: "Switch--label" }, children),
+    h(
+      "span",
+      { className: "Switch-info" },
+      h("span", { className: "Switch--knob" }),
+      switchTextNode
+    )
+  );
+}
+
+export default Switch;
