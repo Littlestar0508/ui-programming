3ed43d7db15954b9e75400ed9deda22921ee7ec0
Littlestar0508 <rist9860@gmail.com>
2025-01-16 10:45:29 +0900

    상태 별 관심사의 분리

--- frontend/components/switch-list.tsx
+++ frontend/components/switch-list.tsx
@@ -18,20 +18,45 @@ function SwitchList({ items }: SwitchListProps) {
   // React 컴포넌트 상태관리
   // React Hooks API(React.useState)
   // true or false 상태
+
+  // [관심사] 과제 제출상태 & 상태 업데이트 로직
   const [submission, setSubmission] = React.useState(false);
+  const handleToggleSubmission = () => {
+    // 상태 업데이트 (React에 요청: trigger a render)
+    const nextSubmission = !submission; // false <=> true
+    setSubmission(nextSubmission);
+  };
+
+  // [관심사] 복습 수행 상태 & 상태 업데이트 로직
+  const [review, setReview] = React.useState(false);
+  const handleToggleReview = () => {
+    // 상태 업데이트 (React에 요청: trigger a render)
+    const nextReview = !review; // false <=> true
+    setReview(nextReview);
+  };
+
+  // [관심사] 나의 수준 파악 상태 & 상태 업데이트 로직
+  const [level, setLevel] = React.useState(false);
+  const handleToggleLevel = () => {
+    const nextLevel = !level;
+    setLevel(nextLevel);
+  };
 
   return (
     <ul className="SwitchList" style={switchStyles}>
       <li>
-        <Switch
-          active={submission}
-          onToggle={() => {
-            // 상태 업데이트 (React에 요청: trigger a render)
-            const nextSubmission = !submission; // false <=> true
-            setSubmission(nextSubmission);
-          }}
-        >
+        <Switch active={submission} onToggle={handleToggleSubmission}>
           과제 제출
+        </Switch>
+      </li>
+      <li>
+        <Switch active={review} onToggle={handleToggleReview}>
+          유의미한 복습 수행
+        </Switch>
+      </li>
+      <li>
+        <Switch active={level} onToggle={handleToggleLevel}>
+          정확한 나의 수준 진단
         </Switch>
       </li>
       {/* 조건부 표시(Conditional Display) */}
