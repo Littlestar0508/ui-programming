0b9eac867d7b993c6ab4c038f70e055964810438
Littlestar0508 <rist9860@gmail.com>
2025-01-17 09:53:23 +0900

    Render Props 패턴 예제

--- frontend/GUIDE/render-props-and-hoc.tsx
+++ frontend/GUIDE/render-props-and-hoc.tsx
@@ -1,4 +1,4 @@
-import React from '../lib/react.js';
+import React from "../lib/react.js";
 
 /* -------------------------------------------------------------------------- */
 /* Render Props & Higher-Order Component                                      */
@@ -8,7 +8,17 @@ export default function RenderPropsAndHOC() {
   return (
     <div className="RenderPropsAndHOC">
       <ReactClassComponent>
-        <ReactFunctionComponent dateInfo={{ iso: '', ko: '' }} />
+        {/* render props 패턴을 사용해 하위 컴포넌트에 dateInfo 정보를 공유합니다. */}
+        {/* JSX === React.ReactElement */}
+        {/* 꼭 자식인 children 속성(prop)이 JSX여야 할까? */}
+        {/* 꼭 JSX일 필요는 없다. 그러면 JavaScript 객체 또는 함수 데이터는 자식이 될 수 없지만... */}
+        {/* 만약 자식으로 전달된 함수가 실행되어 그 결과가 JSX를 반환한다면? 가능합니다! */}
+        {
+          /* render */ (/* 전달된 데이터 받기 가능 */ dateInfo: DateInfo) => {
+            return <ReactFunctionComponent dateInfo={{ iso: dateInfo.iso, ko: dateInfo.ko }} />;
+          }
+        }
+        {/* <ReactFunctionComponent dateInfo={{ iso: "", ko: "" }} /> */}
       </ReactClassComponent>
       <AnotherReactClassComponent />
     </div>
@@ -18,10 +28,9 @@ export default function RenderPropsAndHOC() {
 // 클래스 컴포넌트 --------------------------------------------------------------------
 
 class ReactClassComponent extends React.Component {
+  // this.props지정
   props: {
     children: React.ReactNode;
-    rules?: string[];
-    getRule?: (id: number) => { id: number; content: string }[];
   };
 
   render() {
@@ -31,6 +40,7 @@ class ReactClassComponent extends React.Component {
     return (
       <section>
         <h2>React 규칙 준수</h2>
+        {this.props.children?.(dateInfo)}
       </section>
     );
   }
@@ -59,9 +69,7 @@ function ReactFunctionComponent({ dateInfo }: { dateInfo: DateInfo }) {
   return (
     <>
       <p>규칙을 준수하지 않으면 앱에 문제가 발생합니다.</p>
-      <footer>
-        {dateInfo && <time dateTime={dateInfo.iso}>{dateInfo.ko}</time>}
-      </footer>
+      <footer>{dateInfo && <time dateTime={dateInfo.iso}>{dateInfo.ko}</time>}</footer>
     </>
   );
 }
@@ -70,7 +78,7 @@ function ReactFunctionComponent({ dateInfo }: { dateInfo: DateInfo }) {
 
 function getDateInfo(): DateInfo {
   const d = new Date();
-  const [year, month, date] = d.toLocaleDateString().split('.');
+  const [year, month, date] = d.toLocaleDateString().split(".");
   return {
     iso: d.toISOString(),
     ko: `${year}년 ${month.trim()}월 ${date.trim()}일`,
